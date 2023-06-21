af0ce28298423b0d080e2391eba32575e9fd02e1
ymoldabe <MRanle91@gmail.com>
2023-06-21 22:04:49 +0600

    (((

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 )
+
+type Artist struct {
+	ID            int            `json:"id"`
+	IMAGE         string         `json:"image"`
+	NAME          string         `json:"name"`
+	MEMBERS       map[int]string `json:"members"`
+	CREATION_DATE int            `json:"creationDate"`
+	FIRST_ALBUM   string         `json:"firstAlbum"`
+	LOCATIONS     string         `json:"locations"`
+	CONCERT_DATES string         `json:"concertDates"`
+	RELATIONS     string         `json:"relations"`
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -14,9 +28,31 @@ func main() {
 }
 
 func group(w http.ResponseWriter, r *http.Request) {
+	artistDATA := []Artist{}
+	par := "https://groupietrackers.herokuapp.com/api/artists"
+	mumu, _ := url.Parse(par)
+	jsonCHIK, err := url.ParseQuery(mumu.RawQuery)
+	if err != nil {
+		fmt.Print("sadadsa")
+		return
+	}
+	// defer jsonCHIK.Body.Close()
+	fmt.Println(jsonCHIK)
+	if r.Method != "GET" {
+		Error(w, http.StatusBadRequest)
+		return
+	}
 	if r.URL.Path != "/" {
 		Error(w, 404)
 		return
 	}
-	w.Write([]byte("ooo"))
+	// err := json.Unmarshal([]byte(jsonCHIK), &artistDATA)
+	if err != nil {
+		Error(w, http.StatusInternalServerError)
+		fmt.Println("tutPipec")
+		return
+	}
+	fmt.Println(artistDATA)
 }
+
+// id := r.URL.Query().Get("id")
